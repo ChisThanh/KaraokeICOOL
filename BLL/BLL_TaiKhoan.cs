ad5ddae6041502e0a1c6dd58d8b055544c0de5c2
ChisThanh <chithanh18042003@gmail.com>
2024-06-09 00:26:56 +0700

    Add project files.

---
+++ BLL/BLL_TaiKhoan.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLL_TaiKhoan
+    {
+        DAL_TaiKhoan dal = new DAL_TaiKhoan();
+
+        public List<TaiKhoan> getAllBLL()
+        {
+            return dal.getAll();
+        }
+    }
+}
